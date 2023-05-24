a33a11af1a2ba6739cca4032aa5f3a1ae6b48c7a
Amit-Sharma <86244338+amitxharma@users.noreply.github.com>
2023-05-24 11:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+function sortBandNames(bandNames) {
+  const sortedNames = [];
+
+  bandNames.forEach((name) => {
+    const modifiedName = name.replace(/^(a |an |the )/i, '');
+    sortedNames.push(modifiedName);
+  });
+
+  sortedNames.sort();
+
+  let bandList = '';
+  sortedNames.forEach((name) => {
+    bandList += `<li>${name}</li>`;
+  });
+
+  const bandListContainer = document.querySelector('#band');
+  bandListContainer.innerHTML = bandList;
+}
